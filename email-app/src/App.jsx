f88cef40ff3f6111d2343c19f94eba6df517084f
dezmymachine <desmondkwakucoffie195@gmail.com>
2024-04-11 11:12:38 +0000

    initial frontend installations

---
+++ email-app/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h3 className="text-center text-emerald-900 font-bold">
+        We are going to create an amazing app
+      </h3>
+    </>
+  );
+}
+
+export default App;
